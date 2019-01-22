2c165ca9d0498786054f72819eb512702d015375
rjforty <raaj.ahab@gmail.com>
2019-01-22 23:26:08 +0100

    Added swagger docs generator config

---
+++ config/swagger.js
@@ -0,0 +1,15 @@
+module.exports = {
+    routePrefix: '/documentation',
+    exposeRoute: true,
+    swagger: {
+        info: {
+            title: 'PolyMessenger API',
+            description: 'Web Service to send SMS using Hubtel',
+            version: '1.0.0'
+        },
+        host: 'localhost:3000',
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json']
+    }
+};
