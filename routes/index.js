bcc78b315f45f457c7ea02b3b1a1dc2cb793fc97
rjforty <raaj.ahab@gmail.com>
2019-02-21 09:42:06 +0000

    Added email entry to route
    Signed-off-by: rjforty <raaj.ahab@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const smsController = require("../controllers/sms");
-const smsSchema = require("../schema/sms");
+const {sendSMSSchema} = require("../schema/sms");
+const {sendEmailSchema} = require("../schema/email");
 
 module.exports =  [
     {
         method: "GET",
         url: "/",
-        handler: async (req, res) => {
+        handler: async () => {
             return {hello: "world"}
         }
     },
@@ -13,6 +14,12 @@ module.exports =  [
         method: "POST",
         url: "/api/v1/sms",
         handler: smsController.sendSMS,
-        schema: smsSchema.sendSMSSchema
+        schema: sendSMSSchema
+    },
+    {
+        method: "POST",
+        url: "/api/v1/email",
+        handler: null,
+        schema: sendEmailSchema
     }
 ];
